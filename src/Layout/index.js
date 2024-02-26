72c4de872a6241cb77d4e5730cf6fb030ba793d3
Chris Marquez <chris.yael.m@gmail.com>
2024-02-26 10:26:48 -0700

    editlayout index.js

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -1,14 +1,47 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import deckList from "../components/deck/deckList";
+import createNewDeck from "../components/deck/createNewDeck";
+import Study from "../components/study/study";
+import createNewCard from "../components/cards/createNewCard";
 import Header from "./Header";
 import NotFound from "./NotFound";
+import viewDeck from "../components/deck/viewDeck";
+import editCard from "../components/cards/editCard";
+import editDeck from "../components/deck/editDeck";
 
 function Layout() {
+  
   return (
     <>
       <Header />
       <div className="container">
-        {/* TODO: Implement the screen starting here */}
-        <NotFound />
+      <Switch>
+          <Route exact path="/">
+            <DeckList />
+          </Route>
+          <Route path="/decks/new">
+            <CreateNewDeck />
+          </Route>
+          <Route exact path="/decks/:deckId">
+            <ViewDeck />
+          </Route>
+          <Route path="/decks/:deckId/study">
+            <Study />
+          </Route>
+          <Route path="/decks/:deckId/cards/new">
+            <CreateNewCard />
+          </Route>
+          <Route path="/decks/:deckId/cards/:cardId/edit">
+            <EditCard />
+          </Route>
+          <Route path="/decks/:deckId/edit">
+            <EditDeck />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
       </div>
     </>
   );
